f4586c32ff7d6179f34eb0a23cbfd4fce08c3fc5
Sebastian Cerviño <cervisebas@gmail.com>
2024-05-28 21:00:01 -0300

    add: Proyecto Spring
    Se creo el proyecto desde https://start.spring.io/

---
+++ src/main/java/com/project_itec/cafe/CafeItecApplication.java
@@ -0,0 +1,13 @@
+package com.project_itec.cafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeItecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeItecApplication.class, args);
+	}
+
+}
