828f096acf6e9c0a266b9f4c77daa88e1596eb7f
Sebástian Cerviño <52644268+cervisebas@users.noreply.github.com>
2024-06-11 21:23:51 -0300

    add: Clases Productos, UnidadMedida y TipoProducto
    Se añadio la clase "Productos" con las respectivas clases que la relacionan.

---
+++ src/main/java/com/project_itec/cafe/models/Productos.java
@@ -0,0 +1,35 @@
+package com.project_itec.cafe.models;
+
+public class Productos {
+    protected String nombre;
+    protected String description;
+    protected TipoProducto tipo;
+    protected UnidadMedida unidadMedida;
+    protected Float stock;
+    protected Float precio;
+    protected Boolean borrado;
+
+    public void crearProducto(
+        String _nombre,
+        String _description,
+        TipoProducto _tipo,
+        UnidadMedida _unidadMedida,
+        Float _stock,
+        Float _precio
+    ) {
+        this.nombre = _nombre;
+        this.description = _description;
+        this.tipo = _tipo;
+        this.unidadMedida = _unidadMedida;
+        this.stock = _stock;
+        this.precio = _precio;
+    }
+
+    public Boolean hayStock() {
+        return this.stock != 0;
+    }
+
+    public void borrar() {
+        this.borrado = true;
+    }
+}
---
+++ src/main/java/com/project_itec/cafe/models/TipoProducto.java
@@ -0,0 +1,9 @@
+package com.project_itec.cafe.models;
+
+public class TipoProducto {
+    protected String nombre;
+
+    public void crear(String _nombre) {
+        this.nombre = _nombre;
+    }
+}
